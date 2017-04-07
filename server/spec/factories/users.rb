9006afab8c7f9cbfbdbfbbdd9d4ca8b51d0cf248
= <a.humerickhouse@gmail.com>
2017-04-06 21:53:59 -0500

    adding angular

--- server/spec/factories/users.rb
+++ server/spec/factories/users.rb
@@ -3,5 +3,7 @@ FactoryGirl.define do
     first_name { Faker::Name.first_name }
     last_name { Faker::Name.last_name }
     email { Faker::Internet.email }
+    password 'Test1234'
+    password_confirmation 'Test1234'
   end
 end
