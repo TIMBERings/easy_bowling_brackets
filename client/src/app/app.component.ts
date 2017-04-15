124d898f13c25c56c64c8f31f49c19b74e6d9b64
= <a.humerickhouse@gmail.com>
2017-04-15 12:51:55 -0500

    Angular working

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,7 @@
+// src/app/app.component.ts
+
 import { Component } from '@angular/core';
+import { Http } from '@angular/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  bowlers;
+
+  constructor(private http: Http) {
+    http.get('http://localhost:3000/api/v1/bowlers.json')
+        .subscribe(res => this.bowlers = res.json());
+  }
 }
